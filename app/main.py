d007a0ce09857aa1e1eb60131a47ef33cad82dbe
Albert Lin <Albertkuilin@gmail.com>
2022-01-25 00:38:15 -0500

    finished

--- app/main.py
+++ app/main.py
@@ -3,5 +3,31 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def home_view():
+    return render_template("home.html")
+
+@app.route("/2020-virtual-micromouse-competition/")
 def competition_view():
     return render_template("competition.html")
+
+@app.route("/projects/")
+def projects_view():
+    return render_template("projects.html")
+
+@app.route("/supporters/")
+def supporters_view():
+    return render_template("supporters.html")
+
+@app.route("/join/")
+def join_view():
+    return render_template("join.html")
+
+@app.route("/members/")
+def members_view():
+    return render_template("members.html")
+
+@app.route("/leadership/")
+def leadership_view():
+    return render_template("leadership.html")
+
+    
